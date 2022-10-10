2483a203f3b6dc180922a617f7d7a5d8f2fb0372
selawatiii <2225210072@untirta.ac.id>
2022-10-10 23:29:19 +0800

    main

--- untirta/untirta/urls.py
+++ untirta/untirta/urls.py
@@ -13,9 +13,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path
+from faperta.views import Faperta
+from feb.views import Feb
+from fh.views import Fh
+from fisip.views import Fisip
+from fk.views import Fk
+from fkip.views import Fkip
+from ft.views import Ft
+from pascasarjana.views import Pascasarjana
+from profil.views import Profil
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('faperta/', Faperta, name="faperta"),
+    path('feb/', Feb, name="feb"),
+    path('fh/', Fh, name="fh"),
+    path('fisip/', Fisip, name="fisip"),
+    path('fk/', Fk, name="fk"),
+    path('fkip/', Fkip, name="fkip"),
+    path('ft/', Ft, name="ft"),
+    path('pascasarjana/', Pascasarjana, name="pascasarjana"),
+    path('profil/', Profil, name="profil"),
+    path('', views.index),
 ]
